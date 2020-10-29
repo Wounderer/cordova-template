da3bffdac5aed63470d20dae43e3f72bcb94b0b0
= <=>
2020-10-30 01:00:21 +0300

    Initial commit

---
+++ src/js/routes.js
@@ -0,0 +1,16 @@
+
+import HomePage from '../pages/home.f7.html';
+import HomePage2 from '../pages/home2.f7.html';
+
+var routes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/home2/',
+    component: HomePage2,
+  }
+];
+
+export default routes;
