56b78db7866c7b0ddbbd76649745e9e118877b50
= <=>
2020-11-01 05:38:12 +0300

    pixel perfect home

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+npm config set prefix '~/.npm-global'
 export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=~/.npm-global/bin:$PATH
 export PATH=$PATH:$ANDROID_HOME/tools
 npm update
-framework7 cordova platform rm android
-framework7 cordova platform add android@8.1.0
-npm run build-prod-cordova 
+cd cordova && cordova platform rm android
+cordova platform add android@8.1.0
+cd ..
+npm run build-prod-cordova
 # keytool -genkey -v -keystore key-name.keystore -alias alias-name -keyalg RSA -keysize 2048 -validity 10000
 cd cordova && cordova build android --release
 jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore ../key-name.keystore -storepass somepass platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias-name
 [ -f /tmp/output.apk ] && rm -rf  /tmp/output.apk
 zipalign 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk /tmp/output.apk
 cd ..
+cp -f /tmp/output.apk output.apk
